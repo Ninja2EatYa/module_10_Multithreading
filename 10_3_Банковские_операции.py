dc60f14c41a451a930f08d98e00c7a1ddc3603ed
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-08-10 21:09:25 +0300

    Add files via upload

---
+++ 10_3_Банковские_операции.py
@@ -0,0 +1,43 @@
+import threading
+from random import randint
+from time import sleep
+
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for transactions in range(100):
+            deposit = randint(50, 500)
+            self.balance += deposit
+            print(f'Пополнение: {deposit}. Баланс: {self.balance}')
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            sleep(0.001)
+
+    def take(self):
+        for transactions in range(100):
+            take = randint(50, 500)
+            print(f'Запрос на {take}')
+            if take <= self.balance:
+                self.balance -= take
+                print(f'Снятие: {take}. Баланс: {self.balance}')
+            else:
+                print(f'Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+            sleep(0.001)
+
+
+bk = Bank()
+
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
