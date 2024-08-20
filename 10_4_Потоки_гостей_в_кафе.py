b89f7a44f24fffe6fbff76d02a30adc70e1cfa52
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-08-20 19:44:36 +0300

    10_4_Потоки_гостей_в_кафе.py

--- 10_4_Потоки_гостей_в_кафе.py
+++ 10_4_Потоки_гостей_в_кафе.py
@@ -26,31 +26,27 @@ class Cafe:
 
     def guest_arrival(self, *guests):
         for guest in guests:
-            seated = False
             for table in self.tables:
                 if table.guest is None:
                     table.guest = guest
                     guest.start()
                     print(f'{guest.name} сел(-а) за стол номер {table.number}')
-                    seated = True
                     break
-            if not seated:
+            else:
                 self.queue.put(guest)
                 print(f'{guest.name} в очереди')
 
     def serve_guests(self):
         while not self.queue.empty() or any(table.guest is not None for table in self.tables):
             for table in self.tables:
-                guest = table.guest
-                if guest and not guest.is_alive():
-                    print(f'{guest.name} покушал(-а) и ушёл(ушла)')
+                if table.guest and not table.guest.is_alive():
+                    print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
                     print(f'Стол номер {table.number} свободен')
                     table.guest = None
                     if not self.queue.empty():
-                        next_guest = self.queue.get()
-                        table.guest = next_guest
-                        next_guest.start()
-                        print(f'{next_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
+                        table.guest = self.queue.get()
+                        table.guest.start()
+                        print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
 
 
 # Создание столов
